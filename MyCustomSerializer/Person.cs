aca66c8d7bc6bf5ba8b89c1e8305fb282aab28fe
yanayukhimuk <yana.ryzhikova.brest.belarus@gmail.com>
2023-01-10 00:53:08 +0300

    Implemented  ISerializable

---
+++ MyCustomSerializer/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCustomSerializer
+{
+    [Serializable()]
+    public class Person : ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Person() { }
+
+        public Person(int id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Id", Id);
+            info.AddValue("Name", Name);
+            info.AddValue("Surname", Surname);
+        }
+
+        public Person(SerializationInfo info, StreamingContext context)
+        {
+            Id = info.GetInt32("Id");
+            Name = info.GetString("Name");
+            Surname = info.GetString("Surname");
+        }
+    }
+}
---
+++ MyCustomSerializer/Program.cs
@@ -0,0 +1,24 @@
+﻿using MyCustomSerializer;
+using System.Runtime.Serialization.Formatters.Binary;
+
+Person person1 = new Person(1, "Jane", "Black");
+
+FileStream fileStream = new FileStream(@"C:\Users\Yana_Yukhimuk\source\repos\SQL\Serialization\person.dat", FileMode.Create);
+
+BinaryFormatter formatter = new BinaryFormatter();
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+formatter.Serialize(fileStream, person1);
+fileStream.Close();
+
+FileStream fileStream2 = new FileStream(@"C:\Users\Yana_Yukhimuk\source\repos\SQL\Serialization\person.dat", FileMode.Open);
+
+BinaryFormatter formatter2 = new BinaryFormatter();
+Person deserializedPerson = (Person)formatter.Deserialize(fileStream2);
+
+Console.WriteLine("The deserialized object:");
+Console.WriteLine(String.Format("Id: {0}", deserializedPerson.Id.ToString()));
+Console.WriteLine(String.Format("Name: {0}", deserializedPerson.Name));
+Console.WriteLine(String.Format("Surname: {0}", deserializedPerson.Surname));
+
+Console.Read();
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
