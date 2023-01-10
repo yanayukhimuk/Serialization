b554b0e93aa4687044683041107ebf4d95065028
yanayukhimuk <yana.ryzhikova.brest.belarus@gmail.com>
2023-01-11 00:13:08 +0300

    Implemented deep cloning

---
+++ DeepCloning/Department.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepCloning
+{
+    [Serializable]
+    public class Department : ICloneable
+    {
+        public string? DepartmentName { get; set; }
+        public List<Employee>? EmployeeList { get; set; }
+
+        public Department() { }
+        public Department(string depatmentName = "No name", List<Employee>? emplyeeList = null)
+        {
+            DepartmentName = depatmentName;
+            EmployeeList = emplyeeList;
+        }
+
+        public object Clone()
+        {
+            Department clonedDepartment = (Department)this.MemberwiseClone();
+            clonedDepartment.DepartmentName = DepartmentName;
+            clonedDepartment.EmployeeList = EmployeeList;
+            return clonedDepartment;
+        }
+    }
+}
---
+++ DeepCloning/Program.cs
@@ -0,0 +1,30 @@
+﻿using DeepCloning;
+
+Employee Employee1 = new Employee("Sam Black");
+Employee Employee2 = new Employee("Tom Harrolds");
+Employee Employee3 = new Employee("John Weak");
+Employee Employee4 = new Employee("Sam White");
+
+List<Employee> EmployeeList = new List<Employee>();
+EmployeeList.Add(Employee1);
+EmployeeList.Add(Employee2);
+EmployeeList.Add(Employee3);
+EmployeeList.Add(Employee4);
+
+var department = new Department("MyDepartment", EmployeeList);
+
+Department clonedDepartment = (Department) department.Clone();
+
+department = null;
+
+Employee Employee5 = new Employee("Sam B");
+Employee Employee6 = new Employee("Tom H");
+
+List<Employee> EmployeeList2 = new List<Employee>();
+EmployeeList2.Add(Employee5);
+EmployeeList2.Add(Employee6);
+
+clonedDepartment.EmployeeList= EmployeeList2;
+
+
+Console.WriteLine(clonedDepartment.EmployeeList.Count);
